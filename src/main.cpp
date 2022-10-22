63f41b3a47e204718fbdcf2e3f6bb4d9db919946
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2022-10-22 19:45:25 +0200

    Fix formatting

--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,43 @@
 
 int main(int argc, const char* argv[]) {
 
-    if(!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW library" << std::endl;
-        return 2;
-    }
+  if(!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW library" << std::endl;
+    return 2;
+  }
 
-    glfwWindowHint(GLFW_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_VERSION_MINOR, 0);
-    
-    GLFWwindow* window = glfwCreateWindow(800,600, "ZEN", nullptr, nullptr);
-    if(window == nullptr) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return 2;
-    }
+  glfwWindowHint(GLFW_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_VERSION_MINOR, 0);
 
-    glfwMakeContextCurrent(window);
-
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(0.0f, 0.5f);
-
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex2f(0.5f, -0.5f);
-
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glVertex2f(-0.5f, -0.5f);
-
-        glEnd();
-
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    
-
-    glfwDestroyWindow(window);
+  GLFWwindow* window = glfwCreateWindow(800, 600, "ZEN", nullptr, nullptr);
+  if(window == nullptr) {
+    std::cerr << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
-    return 0;
+    return 2;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while(!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(0.0f, 0.5f);
+
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex2f(0.5f, -0.5f);
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex2f(-0.5f, -0.5f);
+
+    glEnd();
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
 }
