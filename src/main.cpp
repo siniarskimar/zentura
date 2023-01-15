2d365380cb886278818eba0a081434aef7065df8
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2023-01-15 22:36:53 +0100

    Bump OpenGL version used

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ int main(int argc, const char* argv[]) {
   }
 
   glfwWindowHint(GLFW_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
   Window& window = Window::createInstance(800, 600, "ZEN");
