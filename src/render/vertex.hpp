28f9aa66995e6fd48c7e18c9bb046e215cf702f9
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2023-03-13 19:59:51 +0100

    Change Vertex's textureIndex datatype

--- src/render/vertex.hpp
+++ src/render/vertex.hpp
@@ -4,21 +4,23 @@
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
+#include <cstdint>
 
 namespace render {
 
 struct Vertex {
   glm::vec3 position{};
   glm::vec4 color{};
-  float texture{};
+  uint32_t textureIndex{};
   glm::vec2 textureOffset{};
 
   Vertex(glm::vec3 position, glm::vec4 color) : position(position), color(color) {}
 
-  Vertex(glm::vec3 position, glm::vec4 color, float texture, glm::vec2 textureOffset)
+  Vertex(
+      glm::vec3 position, glm::vec4 color, uint32_t textureIdx, glm::vec2 textureOffset)
       : position(position),
         color(color),
-        texture(texture),
+        textureIndex(textureIdx),
         textureOffset(textureOffset) {}
 };
 }
