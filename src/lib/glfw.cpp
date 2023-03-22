7d1afcfe5631a2be60d883f3d97faa86c792fdc8
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2023-03-22 18:29:36 +0100

    Tweak GLFW error callback

--- src/lib/glfw.cpp
+++ src/lib/glfw.cpp
@@ -5,7 +5,15 @@
 
 GLFWLibrary::GLFWLibrary() {
   glfwSetErrorCallback([](const int errorCode, const char* errorMsg) {
+    // "[...] if no error occoured since the last call, the description is set to NULL."
+    if(errorMsg == nullptr) {
+      return;
+    }
+
+    fmt::print(stderr, "GLFW ERROR: {}", errorMsg);
+
     switch(errorCode) {
+      // Fatal errors
     case GLFW_NOT_INITIALIZED:
     case GLFW_NO_CURRENT_CONTEXT:
     case GLFW_NO_WINDOW_CONTEXT:
@@ -15,21 +23,25 @@ GLFWLibrary::GLFWLibrary() {
       throw std::invalid_argument(errorMsg);
     case GLFW_OUT_OF_MEMORY:
       throw std::bad_alloc();
+    // "A bug or configuration error in GLFW, the underlying operating system or its
+    // drivers, or a lack of required resources."
+    case GLFW_PLATFORM_ERROR:
+      throw std::runtime_error("ERROR: GLFW_PLATFORM_ERROR\n");
+
+      // Possibly runtime errors
+      // These can be handled safely
     case GLFW_API_UNAVAILABLE:
       fmt::print(stderr, "ERROR: GLFW_API_UNAVAILABLE\n");
       break;
     case GLFW_VERSION_UNAVAILABLE:
       fmt::print(stderr, "ERROR: GLFW_VERSION_UNAVAILABLE\n");
       break;
-    case GLFW_PLATFORM_ERROR:
-      fmt::print(stderr, "ERROR: GLFW_PLATFORM_ERROR\n");
-      break;
     case GLFW_FORMAT_UNAVAILABLE:
       fmt::print(stderr, "ERROR: GLFW_FORMAT_UNAVAILABLE\n");
       break;
-    default:
-      fmt::print(stderr, "Unknown GLFW error met and std::terminate() called\n");
-      std::terminate();
+    [[unlikely]] default:
+      // This should in theory be unreachable
+      throw std::runtime_error("Unknown GLFW error met");
     }
   });
 
