f576fe7c4572b30982c5eaabacafc3b291193feb
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2024-07-07 12:22:11 +0200

    Print wayland globals

--- build.zig
+++ build.zig
@@ -1,9 +1,17 @@
 const std = @import("std");
 const builtin = @import("builtin");
+const zig_wayland = @import("zig_wayland");
 
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
+
+    const scanner = zig_wayland.Scanner.create(b, .{});
+
+    const mod_wayland = b.createModule(.{ .root_source_file = scanner.result });
+    scanner.addSystemProtocol("stable/xdg-shell/xdg-shell.xml");
+
+    scanner.generate("xdg_wm_base", 3);
 
     const exe = b.addExecutable(.{
         .name = "zentura",
@@ -11,6 +19,9 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    exe.root_module.addImport("wayland", mod_wayland);
     exe.linkLibC();
+    exe.linkSystemLibrary("wayland-client");
+    scanner.addCSource(exe);
     b.installArtifact(exe);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,15 @@
 const std = @import("std");
+const wayland = @import("wayland").client;
+const wl = wayland.wl;
 
-pub fn main() !void {}
+pub fn main() !void {
+    const display = try wl.Display.connect(null);
+    const registry = try display.getRegistry();
+    registry.setListener(?*u8, listener, null);
+    if (display.roundtrip() != .SUCCESS) return error.RoundtripFailed;
+}
+
+fn listener(_: *wl.Registry, event: wl.Registry.Event, data: ?*u8) void {
+    _ = data;
+    std.debug.print("{s}\n", .{event.global.interface});
+}
