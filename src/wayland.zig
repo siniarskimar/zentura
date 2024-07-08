a874133c72d51141d41a79daf59cd6f6f58e3ecd
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2024-07-08 12:48:12 +0200

    Create wayland module

---
+++ src/wayland.zig
@@ -0,0 +1,142 @@
+const std = @import("std");
+const wayland = @import("wayland").client;
+const wl = wayland.wl;
+const xdg = wayland.xdg;
+
+pub const WlContext = struct {
+    wl_display: *wl.Display,
+    wl_registry: *wl.Registry,
+    wl_compositor: ?*wl.Compositor = null,
+    wl_compositor_serial: u32 = 0,
+    xdg_wm_base: ?*xdg.WmBase = null,
+    xdg_wm_base_serial: u32 = 0,
+
+    pub fn init() !@This() {
+        const display = try wl.Display.connect(null);
+        errdefer display.disconnect();
+
+        const registry = try display.getRegistry();
+        errdefer registry.destroy();
+
+        return .{
+            .wl_display = display,
+            .wl_registry = registry,
+        };
+    }
+
+    pub fn deinit(self: *@This()) void {
+        if (self.wl_compositor) |compositor| compositor.destroy();
+        if (self.xdg_wm_base) |wm_base| wm_base.destroy();
+        self.wl_registry.destroy();
+        self.wl_display.disconnect();
+    }
+
+    pub fn globalsListener(registry: *wl.Registry, event: wl.Registry.Event, context: *WlContext) void {
+        switch (event) {
+            .global => |glb| {
+                const interface = std.mem.span(glb.interface);
+
+                if (std.mem.eql(u8, interface, "wl_compositor") and glb.version >= wl.Compositor.generated_version) {
+                    context.wl_compositor = registry.bind(glb.name, wl.Compositor, wl.Compositor.generated_version) catch |err| {
+                        std.debug.panic("failed to bing wl_compositor: {}\n", .{err});
+                    };
+                    context.wl_compositor_serial = glb.name;
+                } else if (std.mem.eql(u8, interface, "xdg_wm_base") and glb.version >= xdg.WmBase.generated_version) {
+                    const wm_base = registry.bind(glb.name, xdg.WmBase, xdg.WmBase.generated_version) catch |err| {
+                        std.debug.panic("failed to bing xdg_wm_base: {}\n", .{err});
+                    };
+                    context.xdg_wm_base = wm_base;
+                    context.xdg_wm_base_serial = glb.name;
+
+                    wm_base.setListener(?*anyopaque, xdgShellPong, null);
+                }
+            },
+            .global_remove => |glb| {
+                if (glb.name == context.wl_compositor_serial) if (context.wl_compositor) |compositor| {
+                    compositor.destroy();
+                    context.wl_compositor = null;
+                    context.wl_compositor_serial = 0;
+                } else if (glb.name == context.xdg_wm_base_serial) if (context.xdg_wm_base) |wm_base| {
+                    wm_base.destroy();
+                    context.xdg_wm_base = null;
+                    context.xdg_wm_base_serial = 0;
+                };
+            },
+        }
+    }
+
+    fn xdgShellPong(wm_base: *xdg.WmBase, event: xdg.WmBase.Event, _: ?*anyopaque) void {
+        wm_base.pong(event.ping.serial);
+    }
+};
+
+pub const WlWindow = struct {
+    context: *WlContext,
+    wl_surface: *wl.Surface,
+    xdg_surface: *xdg.Surface,
+    xdg_toplevel: *xdg.Toplevel,
+    frame_callback: ?*wl.Callback = null,
+
+    // Properties
+    width: u32 = 0,
+    height: u32 = 0,
+    is_closed: bool = false,
+
+    // Synchronization state
+    got_resized: bool = false,
+    surface_should_resize: bool = false,
+
+    pub fn init(context: *WlContext) !@This() {
+        const wm_base = context.xdg_wm_base orelse return error.XdgWmBaseNull;
+        const wl_compositor = context.wl_compositor orelse return error.WlCompositorNull;
+
+        const surface = try wl_compositor.createSurface();
+        errdefer surface.destroy();
+
+        const xdg_surface = try wm_base.getXdgSurface(surface);
+        errdefer xdg_surface.destroy();
+
+        const toplevel = try xdg_surface.getToplevel();
+        errdefer toplevel.destroy();
+
+        return .{
+            .context = context,
+            .wl_surface = surface,
+            .xdg_surface = xdg_surface,
+            .xdg_toplevel = toplevel,
+        };
+    }
+
+    pub fn initListeners(self: *@This()) void {
+        self.xdg_surface.setListener(*@This(), xdgSurfaceHandler, self);
+        self.xdg_toplevel.setListener(*@This(), xdgToplevelHandler, self);
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.xdg_toplevel.destroy();
+        self.xdg_surface.destroy();
+        self.wl_surface.destroy();
+    }
+
+    fn xdgSurfaceHandler(surface: *xdg.Surface, event: xdg.Surface.Event, data: *@This()) void {
+        surface.ackConfigure(event.configure.serial);
+        data.surface_should_resize = data.got_resized;
+    }
+
+    fn xdgToplevelHandler(toplevel: *xdg.Toplevel, event: xdg.Toplevel.Event, data: *@This()) void {
+        _ = toplevel;
+        switch (event) {
+            .configure => |ev| {
+                data.got_resized = false;
+                if (ev.width != 0 and ev.height != 0) {
+                    data.got_resized = true;
+                    data.width = @intCast(ev.width);
+                    data.height = @intCast(ev.height);
+                }
+            },
+            .close => {
+                data.is_closed = true;
+            },
+        }
+    }
+};
