0a2007d3362eb3be1e940cce7e559e2415b911ca
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2024-10-07 10:12:39 +0200

    Use glslang instead of glslc

--- build.zig
+++ build.zig
@@ -55,7 +55,7 @@ pub fn build(b: *std.Build) void {
 
     const shaders = vulkan_zig.ShaderCompileStep.create(
         b,
-        &.{ "glslc", "--target-env=vulkan1.0" },
+        &.{ "glslang", "--target-env=vulkan1.0" },
         "-o",
     );
     shaders.add("triangle_vert", "src/shader/triangle.vert", .{});
