897aed25a7899c26d90e5b6b4d27dc3d0a91878b
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2025-03-23 13:30:27 +0100

    Cleanup vk.zig from unused structs

--- src/vk/InstanceContext.zig
+++ src/vk/InstanceContext.zig
@@ -8,19 +8,19 @@ instance: vk.Instance,
 base_dispatch: vk.BaseDispatch,
 
 const app_info: zvk.ApplicationInfo = .{
-    .p_application_name = "zentura",
     .application_version = 0,
     .engine_version = 0,
     .api_version = zvk.API_VERSION_1_1,
 };
 
-const vkGetInstanceProcAddressFn = vk.vkGetInstanceProcAddressFn;
-
 pub fn init(allocator: std.mem.Allocator, window: *c.SDL_Window) !@This() {
     const getInstanceProcAddress = @as(
-        ?vkGetInstanceProcAddressFn,
+        ?vk.vkGetInstanceProcAddressFn,
         @ptrCast(c.SDL_Vulkan_GetVkGetInstanceProcAddr()),
     ) orelse return error.vkGetInstanceProcAddress;
+
+    var tmp_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer tmp_arena.deinit();
 
     const base_dispatch = try vk.BaseDispatch.load(getInstanceProcAddress);
 
@@ -36,19 +36,24 @@ pub fn init(allocator: std.mem.Allocator, window: *c.SDL_Window) !@This() {
         return error.SDLGetInstanceExtensions;
     }
 
-    const vlayers = if (builtin.mode == .Debug)
-        [_][*:0]const u8{"VK_LAYER_KHRONOS_validation"}
-    else
-        [_][*:0]const u8{};
+    const validation_layers = try ValidationLayers.enumerate(tmp_arena.allocator(), &base_dispatch);
+    if (builtin.mode == .Debug and !validation_layers.VK_LAYER_KHRONOS_validation) {
+        vk.log.warn("VK_LAYER_KHRONOS_validation layer is not present, no vulkan messages will be emited", .{});
+    }
+
+    const vlayers = try validation_layers.asSlice(tmp_arena.allocator());
 
     const instance = try base_dispatch.createInstance(&zvk.InstanceCreateInfo{
         .p_application_info = &app_info,
         .enabled_extension_count = @intCast(sdl_exts.len),
         .pp_enabled_extension_names = @ptrCast(sdl_exts.ptr),
-        .enabled_layer_count = vlayers.len,
-        .pp_enabled_layer_names = &vlayers,
+        .enabled_layer_count = @intCast(vlayers.len),
+        .pp_enabled_layer_names = @ptrCast(vlayers.ptr),
         .flags = .{},
     }, null);
+
+    const vkDestroyInstance: zvk.PfnDestroyInstance = @ptrCast(getInstanceProcAddress(instance, "vkDestroyInstance"));
+    errdefer vkDestroyInstance(instance, null);
 
     const instance_dispatch = try allocator.create(vk.InstanceDispatch);
     errdefer allocator.destroy(instance_dispatch);
@@ -64,3 +69,39 @@ pub fn deinit(self: @This(), allocator: std.mem.Allocator) void {
     self.instance.destroyInstance(null);
     allocator.destroy(self.instance.wrapper);
 }
+
+const ValidationLayers = struct {
+    VK_LAYER_KHRONOS_validation: bool = false,
+
+    fn enumerate(tmp_alocator: std.mem.Allocator, base_dispatch: *const vk.BaseDispatch) !@This() {
+        const layer_properties = try base_dispatch.enumerateInstanceLayerPropertiesAlloc(tmp_alocator);
+        defer tmp_alocator.free(layer_properties);
+
+        var layers: @This() = .{};
+        for (layer_properties) |layer_props| {
+            const layer_name_c: [*:0]const u8 = @ptrCast(&layer_props.layer_name);
+            const layer_name = std.mem.span(layer_name_c);
+
+            // n^2, but n should be small enough that it shouldn't be a big problem
+            inline for (comptime std.meta.fieldNames(@This())) |field_name| {
+                if (std.mem.eql(u8, layer_name, field_name[0..])) {
+                    @field(layers, field_name[0..]) = true;
+                }
+            }
+        }
+        return layers;
+    }
+
+    fn asSlice(self: @This(), allocator: std.mem.Allocator) ![][*:0]const u8 {
+        var layers = std.ArrayList([*:0]const u8).init(allocator);
+        defer layers.deinit();
+
+        inline for (std.meta.fields(@This())) |field| {
+            if (@field(self, field.name)) {
+                try layers.append(field.name);
+            }
+        }
+
+        return try layers.toOwnedSlice();
+    }
+};
